a13625ea55b9627c6abeb7bc269fc48a05d8aa9f
Giovanni Gomez Restrepo Restrepo <giovannyg32@gmail.com>
2023-03-08 12:01:07 -0500

    Se agrega post y get

--- src/application/adapters/controller/service_thedrone_controller.js
+++ src/application/adapters/controller/service_thedrone_controller.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const routes = express.Router();
-const _drones = require('../persistence/medication_persistence');
+const _meditation = require('../persistence/medication_persistence');
+const _drones = require('../persistence/drone_persistence');
+const { Model } = require('../../properties/properties');
 
 routes.get('/drones', (req, res) => {
     const drones = _drones.find();
 
-    if (!drones) res.send({message: 'There is no drones to show'});
+    if (!drones) res.send({ message: 'There is no drones to show' });
 
     res.send(drones);
 });
 
+routes.post('create-dron', (req, res) => {
+    const { name, model, weightLimit, battery, state } = req.body;
 
+    const _newSchema = {
+        name: name, 
+        model: model === Model, 
+        weightLimit: weightLimit, 
+        battery: battery, 
+        state: state
+    }
+
+    const _newDrone = new _drones(_newSchema);
+
+    _newDrone.save();
+})
 
 module.exports = routes;
