a686ef864f3925672d0bdade7ae3c47b2dfe816b
Giovanni Gomez Restrepo Restrepo <giovannyg32@gmail.com>
2023-03-08 16:30:43 -0500

    Medication created, services add

--- src/application/adapters/controller/dispatch_controller.js
+++ src/application/adapters/controller/dispatch_controller.js
@@ -12,9 +12,7 @@ routes.get('/drones', async (req, res) => {
     res.send(drones);
 });
 
-routes.post('/create-dron', async (req, res) => {
-    console.log(req.body);
-    
+routes.post('/create-dron', async (req, res) => {    
     const { name, model, weightLimit, battery, state } = req.body;
 
     const validateModel = validator(model);
@@ -25,7 +23,7 @@ routes.post('/create-dron', async (req, res) => {
 
     const _newSchema = {
         name: name,
-        model: validateModel,
+        model: model,
         weightLimit: weightLimit,
         battery: battery,
         state: state
@@ -38,8 +36,45 @@ routes.post('/create-dron', async (req, res) => {
 
 });
 
-routes.post('/create-medication-drone', (req, res) => {
+routes.post('/load-medication-drone', async (req, res) => {
+    const { idDrone } = req.body;
+
+    if (!idDrone) res.send({
+            message: "Must send id of Drone"
+        })
+
+    const droneById = await _drones.findById(idDrone);
+
+    console.log(droneById);
+
+    const {name, weight, code} = req.body;
+
+    const medicationLoad = {   
+        name: name,
+        weight: weight, 
+        code: code, 
+        droneBy: droneById 
+    }
+
+    console.log(medicationLoad);
+
+    const loadMedication = new _meditation(medicationLoad);
+
+    await loadMedication.save();
+
+    res.send({
+        message: 'medication is loaded'
+    });
 
 });
 
+
+routes.get('/load-medications', async (req, res) => {
+    const medication = await _meditation.find();
+
+    if (!medication) res.send({ message: 'There is no medication to show' });
+
+    res.send(medication);
+
+});
 module.exports = routes;
--- src/application/adapters/persistence/medication_persistence.js
+++ src/application/adapters/persistence/medication_persistence.js
@@ -1,10 +1,11 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, default: mongoose } = require('mongoose');
 
 const _medicationSchema = new Schema({
     name: { type: String, match: /[a-zA-Z0-9_-]$/ },
     weight: { type: Number },
     code: { type: String, match: /[A-Z0-9_-]$/ },
-    image: { data: Buffer, contentType: String }
+    image: { data: Buffer, contentType: String },
+    droneBy: {type: mongoose.Schema.Types.ObjectId, ref: 'Drone'}
 });
 
 module.exports = model('Medication', _medicationSchema);    
